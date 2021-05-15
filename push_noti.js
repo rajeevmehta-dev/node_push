40f606faeb07fa87d3effaefcc17e290f732e652
RAJEEV-HP\Rajeev <rajeev.mehta@outlook.in>
2021-05-15 22:43:13 +0530

    Initial Project setup

---
+++ push_noti.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const webpush = require("web-push");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+// Set static path
+app.use(express.static(path.join(__dirname, "client")));
+
+app.use(bodyParser.json());
+
+const publicVapidKey =
+//  
+
+webpush.setVapidDetails(
+  "mailto:adesh.eddie@gmail.com",
+  publicVapidKey,
+  privateVapidKey
+);
+
+// Subscribe Route
+app.post("/subscribe", (req, res) => {
+  // Get pushSubscription object
+  const subscription = req.body;
+
+  // Send 201 - resource created
+  res.status(201).json({});
+
+  // Create payload
+  const payload = JSON.stringify({ title: "Push Test" });
+
+  // Pass object into sendNotification
+  webpush
+    .sendNotification(subscription, payload)
+    .catch(err => console.error(err));
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
